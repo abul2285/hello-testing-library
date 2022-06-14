652f2101cf00130bf1bc2c5c81dc2b3fb49da17d
Abul Hossain <mdabulhossain780@gmail.com>
2022-06-15 00:18:46 +0600

    feat: stand alone component method mocking tested for VoteBtn compont

---
+++ components/Vote/VoteBtn.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import thumbsUp from './images/thumbs-up.svg';
+import VoteBtn from './VoteBtn';
+
+test('invokes handleVote', () => {
+  const mockHandleVote = jest.fn();
+  render(
+    <VoteBtn
+      handleVote={mockHandleVote}
+      hasVoted={false}
+      imgSrc={thumbsUp}
+      altText='vote like'
+    />
+  );
+
+  user.click(screen.getByRole('button', { name: /vote like/i }));
+  expect(mockHandleVote).toHaveBeenCalled();
+  expect(mockHandleVote).toHaveBeenCalledTimes(1);
+});
